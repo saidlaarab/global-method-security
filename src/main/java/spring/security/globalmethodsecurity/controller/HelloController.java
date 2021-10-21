d2d8a8d7e8597b726610f3c8fe18f017c8058963
Said Laarab <laarabsaid@gmail.com>
2021-10-21 11:07:56 +0100

    Applied "PreAuthorization" to method params

--- src/main/java/spring/security/globalmethodsecurity/controller/HelloController.java
+++ src/main/java/spring/security/globalmethodsecurity/controller/HelloController.java
@@ -2,8 +2,11 @@ package spring.security.globalmethodsecurity.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import spring.security.globalmethodsecurity.service.NameService;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -15,5 +18,10 @@ public class HelloController {
     public String hello(){
         String name = nameService.getName();
         return "Hello " + name + " !";
+    }
+
+    @GetMapping("/secret-names/{username}")
+    public List<String> getUserSecretNames(@PathVariable String username){
+        return nameService.getSecretNamesForUser(username);
     }
 }
--- src/main/java/spring/security/globalmethodsecurity/service/NameService.java
+++ src/main/java/spring/security/globalmethodsecurity/service/NameService.java
@@ -3,11 +3,26 @@ package spring.security.globalmethodsecurity.service;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class NameService {
 
     @PreAuthorize("hasAuthority('write')")
     public String getName(){
         return "Kpixo";
+    }
+
+    private Map<String, List<String>> usersSecretsNames = Map.of(
+            "said", List.of("said-secret-01", "said-secret-02"),
+            "med", List.of("med-secret-01", "med-secret-02")
+    );
+
+    // "#username" --> refers to the parameter of the method
+    // the following annotation has access to the beans of context
+    @PreAuthorize("#username == authentication.principal.username")
+    public List<String> getSecretNamesForUser(String username){
+        return usersSecretsNames.get(username);
     }
 }
